af8182af0bc2feaea5c410c287bebcb15dc5c27a
pashaPasha <zlodei2991@gmail.com>
2018-10-23 17:40:42 +0300

    стиль оформления

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(window).scroll(function(){
 	var st = $(this).scrollTop();
 	$("p, h1, h2, h3").css({
-		"transform":"translate(0%, "+ st /20  + "%"
+		"transform":"translate(0%, "+ st /22  + "%"
 	})
 	});
 
