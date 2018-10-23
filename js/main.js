5c6cc31311fd42dc688f05d462d039cbafea29a2
pashaPasha <zlodei2991@gmail.com>
2018-10-23 08:49:56 +0300

    работа с паралаксом и текстом

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(window).scroll(function(){
 	var st = $(this).scrollTop();
-	$(".header_text").css({
-		"transform":"translate(0%, "+ st   + "%"
+	$("p, h1, h2, h3").css({
+		"transform":"translate(0%, "+ st /20  + "%"
 	})
 	});
